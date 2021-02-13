9a99afdb45f3d28216c9c8e50823759b7c3171f5
sonal <sonalkarle01@gmail.com>
2021-02-13 16:31:11 +0530

    Create link list

---
+++ LinkdedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Programlink
+{
+    class Linklist
+    {
+        static public void Main()
+        {
+            //Create Linked list
+            LinkedList<int> my_list = new LinkedList<int>();
+
+
+            //Add element in linked list
+
+            my_list.AddLast(56);
+            my_list.AddLast(30);
+            my_list.AddLast(70);
+
+            Console.WriteLine("Number as follows");
+            //Access the element
+            foreach(int num in my_list)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
